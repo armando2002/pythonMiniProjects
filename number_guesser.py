19d3d0902b2ec53fbd02951247b90c4bf5394696
Stephen <armando2002@gmail.com>
2022-12-19 20:54:14 -0800

    1st draft of number guessing game

--- number_guesser.py
+++ number_guesser.py
@@ -1,7 +1,7 @@
 import random
 
 # get number input
-top_of_range = input("Type a number: ")
+top_of_range = input("Type a number to generate a random number. You'll then guess the random number: ")
 
 # check number
 if top_of_range.isdigit():
@@ -17,7 +17,29 @@ else:
 
 # generate random # based on max of input
 random_number = random.randrange(0, top_of_range)
-print(random_number)
+guesses = 0
 
+# make sure that input is a number, convert it
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
 
+    else:
+        print("please type a number next time.")
+        # go to top of loop
+        continue
 
+    if user_guess == random_number:
+        # commas let you use a variable easily
+        print("You got it!")
+        break
+    else:
+        # if user guess was bigger or lower, give a hint
+        if user_guess > random_number:
+            print("You were above the number")
+        else:
+            print("You were below the number")
+
+print("You got it in", guesses, "guesses")
