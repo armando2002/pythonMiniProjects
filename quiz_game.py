69386951c96ae863938d996df727788a6d676e70
Stephen <armando2002@gmail.com>
2022-12-19 16:57:40 -0800

    added quiz score

--- quiz_game.py
+++ quiz_game.py
@@ -7,33 +7,41 @@ if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play!")
+score = 0
 
 answer = input("What is the capital of Washington? ")
 if answer.lower() == "olympia":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, wrong answer.")
 
 answer = input("What is the capital of Pennsylvania? ")
 if answer.lower() == "harrisburg":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, wrong answer.")
 
 answer = input("What is the capital of Florida? ")
 if answer.lower() == "tallahassee":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, wrong answer.")
 
 answer = input("What is the capital of California? ")
 if answer.lower() == "sacramento":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, wrong answer.")
 
 answer = input("What is the capital of Hawaii? ")
 if answer.lower() == "honolulu":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, wrong answer.")
+
+print(f"Your got a {str(score)} out of 5.")
